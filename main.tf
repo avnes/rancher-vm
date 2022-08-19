d281b87e8c9febf9ceb886b359a620d23b761fbb
Audun V. Nes <audun.nes@gmail.com>
2022-08-19 21:27:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ module "rancher_vm" {
     project_name  = var.project_name
     network_name  = var.network_name
     domain_name   = var.domain_name
+    custom_cmd    = var.custom_cmd
     nodes         = var.nodes
 }
